c486f2aa72ed75c0ebfabf02549bc55afd1f375a
Sel-Vin Kuik <selvinkuik@gmail.com>
2022-12-10 05:56:34 +0000

    feat: img alt attr

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ function App() {
   return (
     <OverlayScrollbarsComponent defer>
       <div style={{whiteSpace: 'nowrap'}}>
-        <img src="https://dummyimage.com/530x441" />
-        <img src="https://dummyimage.com/530x441" />
-        <img src="https://dummyimage.com/530x441" />
-        <img src="https://dummyimage.com/530x441" />
-        <img src="https://dummyimage.com/530x441" />
-        <img src="https://dummyimage.com/530x441" />
-        <img src="https://dummyimage.com/530x441" />
-        <img src="https://dummyimage.com/530x441" />
+        <img alt="test" src="https://dummyimage.com/530x441" />
+        <img alt="test" src="https://dummyimage.com/530x441" />
+        <img alt="test" src="https://dummyimage.com/530x441" />
+        <img alt="test" src="https://dummyimage.com/530x441" />
+        <img alt="test" src="https://dummyimage.com/530x441" />
+        <img alt="test" src="https://dummyimage.com/530x441" />
+        <img alt="test" src="https://dummyimage.com/530x441" />
+        <img alt="test" src="https://dummyimage.com/530x441" />
       </div>
     </OverlayScrollbarsComponent>
   );
